d0e0225c070c206c5ffb4672531c387c6a9a353c
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-11-22 18:53:16 +0200

    go on

---
+++ js/script18.js
@@ -0,0 +1,11 @@
+///// destructuring
+const isObjToDestruction = {
+    name01: 'Masha',
+    name02: 'Lena',
+    boy: 'Mark',
+    father: 'Abraham',
+    woman: 'Sarah',
+    girl: 'Rebecca',
+};
+const{ name01, boy, woman } = isObjToDestruction;
+console.log(name01, boy, woman);
