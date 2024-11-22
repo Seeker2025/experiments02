8111e55ceb61f001aa48a46996fc9f5537a7ace1
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-11-22 20:00:30 +0200

    go on

--- js/script18.js
+++ js/script18.js
@@ -1,11 +1,35 @@
 ///// destructuring
-const isObjToDestruction = {
-    name01: 'Masha',
-    name02: 'Lena',
-    boy: 'Mark',
-    father: 'Abraham',
-    woman: 'Sarah',
-    girl: 'Rebecca',
+// const isObjToDestruction = {
+//     name01: 'Masha',
+//     name02: 'Lena',
+//     boy: 'Mark',
+//     father: 'Abraham',
+//     woman: 'Sarah',
+//     girl: 'Rebecca',
+// };
+// const{ name01, boy, woman } = isObjToDestruction;
+// console.log(name01, boy, woman);
+
+const formEl = document.querySelector('.js-form');
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const { userAge, userEmail, userName } = evt.currentTarget.elements;
+    // console.dir(evt.currentTarget);
+    // console.log(userAge.value);
+    // console.log(userEmail.value);
+    // console.log(userName.value);
+    
+    const data = {
+    name: userName.value,
+    email: userEmail.value,
+    age: userAge.value,
 };
-const{ name01, boy, woman } = isObjToDestruction;
-console.log(name01, boy, woman);
+console.log(data);
+}
+
+
+
+
+
