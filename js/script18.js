bd9ccea1d40a287310f9db41c5f057203b5adfe7
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-11-30 11:37:03 +0200

    go on

--- js/script18.js
+++ js/script18.js
@@ -34,30 +34,48 @@
 // console.log(data);
 // }
 
-const container = document.querySelector('.js-container');
-container.addEventListener('click', onClick);
 
-function onClick(event02) {
-    if (event02.target.nodeName !== 'BUTTON') {
+////// Butons
+// const container = document.querySelector('.js-container');
+// container.addEventListener('click', onClick);
+// function onClick(event02) {
+//     if (event02.target.nodeName !== 'BUTTON') {
+//         return;
+//     }
+//     // console.log(event02.target);
+//     // console.log(event02.currentTarget);
+//     const currentActiveBtn = document.querySelector('.button_style');
+//     if (currentActiveBtn) {
+//        currentActiveBtn.classList.remove('button_style');
+//     }
+//     event02.target.classList.add('button_style');
+// }
+
+
+
+
+
+
+
+
+
+
+////// Buttons Repeating
+const box_button = document.querySelector('.js_box_button');
+// console.log(box_button);
+box_button.addEventListener('click', clickOnBox);
+function clickOnBox(event03) {
+    // console.log(event03.target);
+
+    if (event03.target.nodeName !== 'BUTTON') {
         return;
     }
-    // console.log(event02.target);
-    // console.log(event02.currentTarget);
-    const currentActiveBtn = document.querySelector('.button_style');
-    if (currentActiveBtn) {
-       currentActiveBtn.classList.remove('button_style');
-    }
-    event02.target.classList.add('button_style');
+    // else{console.log('Hi!')}
+    const activeButton = document.querySelector('.button_style');
+    // if (activeButton) {
+    //     activeButton.classList.remove('button_style');
+    // }
+
+    event03.target.classList.toggle('button_style');
 }
-
-
-
-
-
-
-
-
-
-
-
 
